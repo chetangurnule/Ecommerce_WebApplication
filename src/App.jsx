0d284165b4f6908c1f79b08b08986244a982ec23
chetangurnule <chetangurnule99@gmail.com>
2024-02-21 17:50:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-white bg-slate-900 text-3xl">
+        Welcome to the E-commerce webApplication
+      </h1>
+    </>
+  );
+}
+
+export default App;
